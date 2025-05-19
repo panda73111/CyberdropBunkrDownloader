93004b54f436103b77ef853f393a25b6155957bc
Sebastian Hüther <sebastian.huether@gmx.de>
2025-05-19 12:03:57 +0200

    converted to asyncronous requests

--- dump.py
+++ dump.py
@@ -1,131 +1,136 @@
-import time
-import requests
-import json
 import argparse
-import sys
+import asyncio
 import os
 import re
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-from tqdm import tqdm
+import sys
+import time
 from base64 import b64decode
 from math import floor
+from urllib.parse import urlparse
+
+import requests
+from aiohttp import ClientSession
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 BUNKR_VS_API_URL_FOR_SLUG = "https://bunkr.cr/api/vs"
 SECRET_KEY_BASE = "SECRET_KEY_"
 
-def get_items_list(session, url, retries, extensions, only_export, custom_path=None):
+
+async def get_items_list(session: ClientSession, url: str, retries: int, extensions, only_export, custom_path=None):
     extensions_list = extensions.split(',') if extensions is not None else []
-       
-    r = session.get(url)
-    if r.status_code != 200:
-        raise Exception(f"[-] HTTP error {r.status_code}")
 
-    soup = BeautifulSoup(r.content, 'html.parser')
-    is_bunkr = "| Bunkr" in soup.find('title').text
+    async with session.get(url) as response:
+        if response.status != 200:
+            raise Exception(f"[-] HTTP error {response.status}")
 
-    direct_link = False
-    
-    if is_bunkr:
-        items = []
-        soup = BeautifulSoup(r.content, 'html.parser')
+        response_text = await response.text()
+        soup = BeautifulSoup(response_text, 'html.parser')
+        is_bunkr = "| Bunkr" in soup.find('title').text
 
-        direct_link = soup.find('span', {'class': 'ic-videos'}) is not None or soup.find('div', {'class': 'lightgallery'}) is not None
-        if direct_link:
-            album_name = soup.find('h1', {'class': 'text-[20px]'})
-            if album_name is None:
-                album_name = soup.find('h1', {'class': 'truncate'})
+        direct_link = False
 
-            album_name = remove_illegal_chars(album_name.text)
-            items.append(get_real_download_url(session, url, True))
-        else:
-            boxes = soup.find_all('a', {'class': 'after:absolute'})
-            for box in boxes:
-                items.append({'url': box['href'], 'size': -1})
-            
-            album_name = soup.find('h1', {'class': 'truncate'}).text
-            album_name = remove_illegal_chars(album_name)
-    else:
-        items = []
-        items_dom = soup.find_all('a', {'class': 'image'})
-        for item_dom in items_dom:
-            items.append({'url': f"https://cyberdrop.me{item_dom['href']}", 'size': -1})
-        album_name = remove_illegal_chars(soup.find('h1', {'id': 'title'}).text)
+        if is_bunkr:
+            items = []
+            soup = BeautifulSoup(response_text, 'html.parser')
 
-    download_path = get_and_prepare_download_path(custom_path, album_name)
-    already_downloaded_url = get_already_downloaded_url(download_path)
+            direct_link = soup.find('span', {'class': 'ic-videos'}) is not None or soup.find('div', {
+                'class': 'lightgallery'}) is not None
+            if direct_link:
+                album_name = soup.find('h1', {'class': 'text-[20px]'})
+                if album_name is None:
+                    album_name = soup.find('h1', {'class': 'truncate'})
 
-    for item_index, item in enumerate(items):
-        if not direct_link:
-            orig_url = item['url']
-            item = get_real_download_url(session, item['url'], is_bunkr)
-            if item['url'] == '/':
-                print(f"unable to find a download link for file https://bunkr.si{orig_url}")
-                continue
-            if item is None:
-                print(f"[-] Unable to find a download link")
-
-        extension = get_url_data(item['url'])['extension']
-        if ((extension in extensions_list or len(extensions_list) == 0) and (item['url'] not in already_downloaded_url)):
-            if only_export:
-                write_url_to_list(item['url'], download_path)
+                album_name = remove_illegal_chars(album_name.text)
+                item = await get_real_download_url(session, url, True)
+                items.append(item)
             else:
-                for i in range(1, retries + 1):
-                    try:
-                        print(f"[+] Downloading {item['url']} (try {i}/{retries})")
-                        download(session, item['url'], download_path, is_bunkr, item['name'] if not is_bunkr else None)
-                        break
-                    except requests.exceptions.ConnectionError as e:
-                        if i < retries:
-                            time.sleep(2)
-                            pass
-                        else:
-                            raise e
+                boxes = soup.find_all('a', {'class': 'after:absolute'})
+                for box in boxes:
+                    items.append({'url': box['href'], 'size': -1})
 
-    print(f"[+] File list exported in {os.path.join(download_path, 'url_list.txt')}" if only_export else f"[+] Download completed")
-    return
-    
-def get_real_download_url(session, url, is_bunkr=True):
+                album_name = soup.find('h1', {'class': 'truncate'}).text
+                album_name = remove_illegal_chars(album_name)
+        else:
+            items = []
+            items_dom = soup.find_all('a', {'class': 'image'})
+            for item_dom in items_dom:
+                items.append({'url': f"https://cyberdrop.me{item_dom['href']}", 'size': -1})
+            album_name = remove_illegal_chars(soup.find('h1', {'id': 'title'}).text)
 
+        download_path = get_and_prepare_download_path(custom_path, album_name)
+        already_downloaded_url = get_already_downloaded_url(download_path)
+
+        for item_index, item in enumerate(items):
+            if not direct_link:
+                orig_url = item['url']
+                item = await get_real_download_url(session, item['url'], is_bunkr)
+                if item['url'] == '/':
+                    print(f"unable to find a download link for file https://bunkr.si{orig_url}")
+                    continue
+                if item is None:
+                    print(f"[-] Unable to find a download link")
+
+            extension = get_url_data(item['url'])['extension']
+            if ((extension in extensions_list or len(extensions_list) == 0) and (
+                    item['url'] not in already_downloaded_url)):
+                if only_export:
+                    write_url_to_list(item['url'], download_path)
+                else:
+                    for i in range(1, retries + 1):
+                        try:
+                            print(f"[+] Downloading {item['url']} (try {i}/{retries})")
+                            await download(session, item['url'], download_path, is_bunkr,
+                                           item['name'] if not is_bunkr else None)
+                            break
+                        except requests.exceptions.ConnectionError as e:
+                            if i < retries:
+                                time.sleep(2)
+                                pass
+                            else:
+                                raise e
+
+        print(
+            f"[+] File list exported in {os.path.join(download_path, 'url_list.txt')}" if only_export else f"[+] Download completed")
+        return
+
+
+async def get_real_download_url(session: ClientSession, url, is_bunkr=True):
     if is_bunkr:
         url = url if 'https' in url else f'https://bunkr.si{url}'
     else:
-        url = url.replace('/f/','/api/f/')
+        url = url.replace('/f/', '/api/f/')
 
-    r = session.get(url)
-    if r.status_code != 200:
-        print(f"[-] HTTP error {r.status_code} getting real url for {url}")
-        return None
+    async with session.get(url) as response:
+        if response.status != 200:
+            print(f"[-] HTTP error {response.status} getting real url for {url}")
+            return None
 
-    if is_bunkr:
-        slug = re.search(r'\/f\/(.*?)$', url).group(1)
-        decrypted_url = decrypt_encrypted_url(get_encryption_data(slug))
-        return {'url': decrypted_url, 'size': -1}
-        # soup = BeautifulSoup(r.content, 'html.parser')
-        # videoElement = soup.find("video")
-        # videoSource = videoElement.get("currentSrc")
-        # videoName = soup.find("meta", property="og:title")
-        # return {'url': videoSource, 'size': -1, 'name': videoName}
-    else:
-        item_data = json.loads(r.content)
-        return {'url': item_data['url'], 'size': -1, 'name': item_data['name']}
+        if is_bunkr:
+            slug = re.search(r'\/f\/(.*?)$', url).group(1)
+            encryption_data = await get_encryption_data(session, slug)
+            decrypted_url = decrypt_encrypted_url(encryption_data)
+            return {'url': decrypted_url, 'size': -1}
+        else:
+            item_data = await response.json()
+            return {'url': item_data['url'], 'size': -1, 'name': item_data['name']}
 
-def download(session, item_url, download_path, is_bunkr=False, file_name=None):
 
+async def download(session: ClientSession, item_url, download_path, is_bunkr=False, file_name=None):
     file_name = get_url_data(item_url)['file_name'] if file_name is None else file_name
     final_path = os.path.join(download_path, file_name)
 
-    with session.get(item_url, stream=True, timeout=5) as r:
-        if r.status_code != 200:
-            print(f"[-] Error downloading \"{file_name}\": {r.status_code}")
+    async with session.get(item_url, timeout=5) as response:
+        if response.status != 200:
+            print(f"[-] Error downloading \"{file_name}\": {response.status}")
             return
-        if r.url == "https://bnkr.b-cdn.net/maintenance.mp4":
+        if response.url == "https://bnkr.b-cdn.net/maintenance.mp4":
             print(f"[-] Error downloading \"{file_name}\": Server is down for maintenance")
 
-        file_size = int(r.headers.get('content-length', -1))
+        file_size = int(response.headers.get('content-length', -1))
         with open(final_path, 'wb') as f:
             with tqdm(total=file_size, unit='iB', unit_scale=True, desc=file_name, leave=False) as pbar:
-                for chunk in r.iter_content(chunk_size=8192):
+                async for chunk in response.content.iter_chunked(8192):
                     if chunk is not None:
                         f.write(chunk)
                         pbar.update(len(chunk))
@@ -140,20 +145,24 @@ def download(session, item_url, download_path, is_bunkr=False, file_name=None):
 
     return
 
-def create_session():
-    session = requests.Session()
-    session.headers.update({
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+
+async def create_session():
+    session = ClientSession(headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/133.0.0.0 Safari/537.36',
         'Referer': 'https://bunkr.si/',
     })
     return session
 
+
 def get_url_data(url):
     parsed_url = urlparse(url)
-    return {'file_name': os.path.basename(parsed_url.path), 'extension': os.path.splitext(parsed_url.path)[1], 'hostname': parsed_url.hostname}
+    return {'file_name': os.path.basename(parsed_url.path), 'extension': os.path.splitext(parsed_url.path)[1],
+            'hostname': parsed_url.hostname}
+
 
 def get_and_prepare_download_path(custom_path, album_name):
-
     final_path = 'downloads' if custom_path is None else custom_path
     final_path = os.path.join(final_path, album_name) if album_name is not None else 'downloads'
     final_path = final_path.replace('\n', '')
@@ -168,8 +177,8 @@ def get_and_prepare_download_path(custom_path, album_name):
 
     return final_path
 
-def write_url_to_list(item_url, download_path):
 
+def write_url_to_list(item_url, download_path):
     list_path = os.path.join(download_path, 'url_list.txt')
 
     with open(list_path, 'a', encoding='utf-8') as f:
@@ -177,38 +186,39 @@ def write_url_to_list(item_url, download_path):
 
     return
 
-def get_already_downloaded_url(download_path):
 
+def get_already_downloaded_url(download_path):
     file_path = os.path.join(download_path, 'already_downloaded.txt')
 
     if not os.path.isfile(file_path):
         return []
-    
+
     with open(file_path, 'r', encoding='utf-8') as f:
         return f.read().splitlines()
 
-def mark_as_downloaded(item_url, download_path):
 
+def mark_as_downloaded(item_url, download_path):
     file_path = os.path.join(download_path, 'already_downloaded.txt')
     with open(file_path, 'a', encoding='utf-8') as f:
         f.write(f"{item_url}\n")
 
     return
 
+
 def remove_illegal_chars(string):
     return re.sub(r'[<>:"/\\|?*\']|[\00-\031]', "-", string).strip()
 
-def get_encryption_data(slug=None):
 
-    r = session.post(BUNKR_VS_API_URL_FOR_SLUG, json={'slug': slug})
-    if r.status_code != 200:
-        print(f"[-] HTTP ERROR {r.status_code} getting encryption data")
-        return None
-    
-    return json.loads(r.content)
+async def get_encryption_data(session: ClientSession, slug=None):
+    async with session.post(BUNKR_VS_API_URL_FOR_SLUG, json={'slug': slug}) as response:
+        if response.status != 200:
+            print(f"[-] HTTP ERROR {response.status} getting encryption data")
+            return None
+
+        return await response.json()
+
 
 def decrypt_encrypted_url(encryption_data):
-
     secret_key = f"{SECRET_KEY_BASE}{floor(encryption_data['timestamp'] / 3600)}"
     encrypted_url_bytearray = list(b64decode(encryption_data['url']))
     secret_key_byte_array = list(secret_key.encode('utf-8'))
@@ -219,12 +229,14 @@ def decrypt_encrypted_url(encryption_data):
         decrypted_url += chr(encrypted_url_bytearray[i] ^ secret_key_byte_array[i % len(secret_key_byte_array)])
 
     return decrypted_url
-    
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(sys.argv[1:])
     parser.add_argument("-u", help="Url to fetch", type=str, required=False, default=None)
     parser.add_argument("-f", help="File to list of URLs to download", required=False, type=str, default=None)
-    parser.add_argument("-r", help="Amount of retries in case the connection fails", type=int, required=False, default=10)
+    parser.add_argument("-r", help="Amount of retries in case the connection fails", type=int, required=False,
+                        default=10)
     parser.add_argument("-e", help="Extensions to download (comma separated)", type=str)
     parser.add_argument("-p", help="Path to custom downloads folder")
     parser.add_argument("-w", help="Export url list (ex: for wget)", action="store_true")
@@ -240,15 +252,26 @@ if __name__ == '__main__':
         print("[-] Please provide only one URL or file")
         sys.exit(1)
 
-    session = create_session()
+    loop = asyncio.get_event_loop()
+    session = loop.run_until_complete(create_session())
 
     if args.f is not None:
         with open(args.f, 'r', encoding='utf-8') as f:
             urls = f.read().splitlines()
         for url in urls:
             print(f"[-] Processing \"{url}\"...")
-            get_items_list(session, url, args.r, args.e, args.w, args.p)
+            func = get_items_list(session, url, args.r, args.e, args.w, args.p)
+            try:
+                loop.run_until_complete(func)
+                loop.run_until_complete(session.close())
+            except KeyboardInterrupt:
+                pass
         sys.exit(0)
     else:
-        get_items_list(session, args.u, args.r, args.e, args.w, args.p)
+        func = get_items_list(session, args.u, args.r, args.e, args.w, args.p)
+        try:
+            loop.run_until_complete(func)
+            loop.run_until_complete(session.close())
+        except KeyboardInterrupt:
+            pass
     sys.exit(0)
